159bfe68cfa0c9334f0adacba28d449673cc49b1
Bilal <yyybilal@gmail.com>
2021-12-30 09:48:22 +0100

    Initial Commit

---
+++ MVVM Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM Combine
+//
+//  Created by bilal on 30/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM Combine/MVVM_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_CombineApp.swift
+//  MVVM Combine
+//
+//  Created by bilal on 30/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
